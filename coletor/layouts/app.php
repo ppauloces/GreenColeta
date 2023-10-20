90bc441e871505a7ad32cde995e76052466d7b54
Paulo Amaral <ppaulo.developer@gmail.com>
2023-10-19 21:15:30 -0300

    origin

--- coletor/layouts/app.php
+++ coletor/layouts/app.php
@@ -11,14 +11,14 @@
         </div>
     </nav>
     <ul class="sidenav" id="mobile-demo">
-        <li><a href="<?= URL . 'dados.php' ?>" style="color: #538D22"><i class="material-icons left" style="margin-right: 5px;color: #538D22">info_outline</i>Dados</a></li>
+        <li><a href="<?= URL . '' ?>" style="color: #538D22"><i class="material-icons left" style="margin-right: 5px;color: #538D22">info_outline</i>Dados</a></li>
         <li><a href="historico.php" style="color: #538D22"><i class="material-icons left" style="margin-right: 5px;color: #538D22">history</i>Histórico</a></li>
         <li class="divider"></li>
         <li><a href="<?= URL . 'config/logout.php' ?>" style="color: red"><i class="material-icons left" style="margin-right: 5px;color: red">exit_to_app</i>Sair</a></li>
     </ul>
     <!-- Dropdown Structure -->
     <ul id="dropdown1" class="dropdown-content">
-        <li><a href="<?= URL . 'dados.php' ?>" style="color: #538D22"><i class="material-icons left" style="margin-right: 5px;color: #538D22">info_outline</i>Dados</a></li>
+        <li><a href="<?= URL . '' ?>" style="color: #538D22"><i class="material-icons left" style="margin-right: 5px;color: #538D22">info_outline</i>Dados</a></li>
         <li><a href="historico.php" style="color: #538D22"><i class="material-icons left" style="margin-right: 5px;color: #538D22">history</i>Histórico</a></li>
         <li class="divider"></li>
         <li><a href="<?= URL . 'config/logout.php' ?>" style="color: red"><i class="material-icons left" style="margin-right: 5px;color: red">exit_to_app</i>Sair</a></li>
